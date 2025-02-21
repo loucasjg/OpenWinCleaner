dc1f19e5b0709625d167e95cdb19842f352ce3f6
loucasjg <142547250+loucasjg@users.noreply.github.com>
2025-02-21 11:10:08 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell -Command "Start-Process python -ArgumentList 'nettoyage_pc.py' -Verb RunAs"
+powershell -Command "Start-Process python -ArgumentList 'OpenWinCleaner.py' -Verb RunAs"
 exit
